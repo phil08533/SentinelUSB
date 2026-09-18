71a3d2807d8b4ee189ca12690e39358352be6144
Philip Gonzales <philip08533gonzales@gmail.com>
2026-09-17 22:59:58 -0400

    Begin minimal live image build

---
+++ live-build/config/includes.chroot/usr/local/lib/sentinelusb/cli.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+BANNER = r'''\n   _____            __  _ __      __ __  ______  ____\n  / ___/__  ____   / /_(_) /___ _/ // / / __/ / / / /\n  \\__ \\/ _ \\/ __ \\ / __/ / / __ `/ // /_/ /_/ / /\n ___/ /  __/ / / // /_/ / / /_/ /__  __/ __/ __  /\n/____/\\___/_/ /_/\\__/_/_/\\__,_/  /_/ /_/ /_/ /_/\n                 S E N T I N E L\n'''
+print(BANNER)
+print('  SentinelUSB Security Rescue Environment')
+print('  ----------------------------------------')
+print("  Type 'help' for available commands.")
